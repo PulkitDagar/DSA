7d533abc46167308d9a57f25093cedd1bc26a80f
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-02 21:56:52 +0530

    updated

--- Recursion/findKey.cpp
+++ Recursion/findKey.cpp
@@ -1,18 +1,32 @@
 #include<iostream>
 using namespace std;
 
-bool checkKey(string& str, int i, int& n, char& key){
-    
+bool checkKey(string str, int i, int n, char key){
+    //base case
+    if(i>=n){
+        return false;
+    }
+
+    //recursive case
+    if(str[i] == key){
+        return true;
+    }
+
+    //baaki recursion sambhal lega
+    bool ans = checkKey(str,i+1,n,key);
+    return ans;
 }
 int  main(){
 
     ///find the given key in an string
-    string str = "PulkitDagar";
+    string str = "Pulkitdagar";
     int n = str.length();
-    char key = 't';
+    char key = 'a';
 
     int i = 0;
     bool ans = checkKey(str,n,i,key);
+
+    cout<<"answer will be"<<ans<<endl;
 
     return 0;
 }
