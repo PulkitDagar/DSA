0431f34e914866b6b4dc1b6acff8b02191bddd8b
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-02 21:30:48 +0530

    finding the key in given string

---
+++ Recursion/findKey.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool checkKey(string& str, int i, int& n, char& key){
+    
+}
+int  main(){
+
+    ///find the given key in an string
+    string str = "PulkitDagar";
+    int n = str.length();
+    char key = 't';
+
+    int i = 0;
+    bool ans = checkKey(str,n,i,key);
+
+    return 0;
+}
