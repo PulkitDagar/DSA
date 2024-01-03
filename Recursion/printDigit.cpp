002b5a4b39ec0a638b4572321904b8bfa61dee26
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-03 09:51:09 +0530

    printdigits using Recursion

--- Recursion/printDigit.cpp
+++ Recursion/printDigit.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 void printDigit(int n){
-    
+    ///base case
+    if(n==0){
+        return;
+    }
 
+    int newValOfN = n/10;
+    printDigit(newValOfN);
+
+    int digit = n%10;
+    cout<<digit<<" ";
 
 }
 int main(){
