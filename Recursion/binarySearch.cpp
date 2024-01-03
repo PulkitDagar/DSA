2413a155588f7c197753aecb2d4341dd71f75e3a
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-03 10:24:54 +0530

    binarySearch using Recursion

--- Recursion/binarySearch.cpp
+++ Recursion/binarySearch.cpp
@@ -17,18 +17,25 @@ int binarySearch(vector<int> arr, int start, int end, int target){
 
     ///baaki recursion sambhal lega
 
+    if(arr[mid]<target){
+        return binarySearch(arr, mid+1,end,target);
+    }
+    else{
+        return  binarySearch(arr,start,mid-1, target);
+    }
+   
 
 };
 
 int main(){
     vector<int> v{10,20,30,40,60,70,90,99};
     int n = v.size();
-    int target = 90;
+    int target = 99;
 
     int start = 0;
     int end = n-1;
 
-    int ans = binarySearch(v,end,target,start);
+    int ans = binarySearch(v,start,end,target);
 
     cout<<" The index at which our target is present is "<<ans <<endl;
 
