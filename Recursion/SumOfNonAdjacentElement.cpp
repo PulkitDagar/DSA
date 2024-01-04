2fac9a612b26965cb674563a3a232331adaab0b5
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-04 22:45:31 +0530

    updated

--- Recursion/SumOfNonAdjacentElement.cpp
+++ Recursion/SumOfNonAdjacentElement.cpp
@@ -3,13 +3,19 @@
 #include<limits.h>
 using namespace std;
 
-void solve(vector<int>& arr, int i, int sum, int &maxi){
+void solve(vector<int> &arr, int i, int sum, int &maxi){
     //base case
-    if(i>arr.size()){
+    if(i>=arr.size()){
         maxi = max(sum,maxi);
         return;
     }
     //ek case solve kro baaki recursion sambhal lega
+    //include
+    solve(arr,i+2,sum+arr[i], maxi);
+
+    //exclude
+    solve(arr,i+1,sum,maxi);
+
 }
 int main(){
     vector<int>arr{1,2,3,5,4};
