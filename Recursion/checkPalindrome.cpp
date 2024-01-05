0c97120edc51ebc48022177c049038156f7aad8e
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-05 17:31:34 +0530

    palindrome

--- Recursion/checkPalindrome.cpp
+++ Recursion/checkPalindrome.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
-int main(){
+bool isPalindrome(string &s, int start, int end){
 
+    
+}
+
+int main(){
+    string s;
+    cin>>s;
+
+    cout<<isPalindrome(s,0,s.size()-1)<<endl;
 
     return 0;
 }
