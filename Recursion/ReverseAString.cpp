78719fb4d6860a640f117895e2fbd9b687f3a67d
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-05 17:27:43 +0530

    Important question

--- Recursion/ReverseAString.cpp
+++ Recursion/ReverseAString.cpp
@@ -2,8 +2,27 @@
 #include<string>
 using namespace std;
 
-int main(){
+void reverse(string &s, int start, int end){
+    //base case
+    if(start>=end){
+        return;
+    }
 
+    //ek case solve krdo baaki recursion sambhal lega
+    swap(s[start], s[end]);
+
+    reverse(s,start+1,end-1);
+
+}
+
+int main(){
+    string s;
+    cin>>s;
+
+    reverse(s,0,s.size()-1);
+
+    cout<<s<<endl;
+    
 
     return 0;
 }
