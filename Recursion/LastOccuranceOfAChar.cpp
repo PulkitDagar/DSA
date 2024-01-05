a5d0aa4d626a5991a948a084613f5d22d1696b28
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-05 17:18:02 +0530

    Recursion

--- Recursion/LastOccuranceOfAChar.cpp
+++ Recursion/LastOccuranceOfAChar.cpp
@@ -5,8 +5,21 @@
 #include<string>
 using namespace std;
 
-int main(){
+void lastOccLTR(string &s, char x, int i, int &ans){
 
+    
+}
+
+int main(){
+    string s;
+    cin>>s;
+    char x;
+    cin>>x;
+
+    int ans = -1;
+    lastOccLTR(s,x,0,ans);
+
+    cout<<ans<<endl;
 
 
     return 0;
