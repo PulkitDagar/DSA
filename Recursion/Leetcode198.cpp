afbf3067c93419831aa22707cbb31608c0ae598c
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-06 09:49:02 +0530

    House Robber

--- Recursion/Leetcode198.cpp
+++ Recursion/Leetcode198.cpp
@@ -1,1 +1,19 @@
 //House Robber Problem
+class Solution {
+public:
+
+    int robHelper(vector<int>&nums,int i){
+        //base case
+        if(i>=nums.size()){
+            return 0;
+        }
+
+        int robAmt1 = nums[i] + robHelper(nums,i+2);
+        int robAmt2 = 0 + robHelper(nums,i+1);
+        return max(robAmt1,robAmt2);
+    }
+
+    int rob(vector<int>& nums) {
+        return robHelper(nums,0);
+    }
+};
