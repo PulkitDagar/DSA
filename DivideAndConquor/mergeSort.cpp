208b62d666cf7b7b5d70d5e4b98588fc6799751b
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-09 11:38:16 +0530

    merge sort using recursion

--- DivideAndConquor/mergeSort.cpp
+++ DivideAndConquor/mergeSort.cpp
@@ -3,6 +3,63 @@ using namespace std;
 
 void merge(int* arr, int start, int end){
     //now we have to create two new arrays
+    int mid = start+(end-start)/2;
+
+    int len1 = mid-start+1;
+    int len2 = end-mid;
+
+    //now we creae 2 arrays for lengthl1 and lengthl2
+    int* left = new int[len1];
+    int* right = new int[len2];
+
+    // copy values
+    int k = start;
+    for(int i = 0; i<len1; i++){
+        left[i] = arr[k];
+        k++;
+    }
+
+    k = mid+1;
+    for(int i=0; i<len2; i++){
+        right[i] = arr[k];
+        k++;
+    };
+
+    //merge logic
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int mainArrayIndex = start;
+
+    while(leftIndex<len1 && rightIndex<len2){
+
+        if(left[leftIndex]<right[rightIndex]){
+            arr[mainArrayIndex] = left[leftIndex];
+            mainArrayIndex++;
+            leftIndex++;
+        }
+
+        else{
+            arr[mainArrayIndex] = right[rightIndex];
+            mainArrayIndex++;
+            rightIndex++;
+        }
+    };
+
+
+    ///if the element are reached in one of the array so copy othe elements of array directly
+    while(leftIndex<len1){
+        arr[mainArrayIndex] = left[leftIndex];
+        mainArrayIndex++;
+        leftIndex++;
+    }
+
+    while(rightIndex<len2){
+        arr[mainArrayIndex] = right[rightIndex];
+        mainArrayIndex++;
+        rightIndex++;
+    }
+
+
 }
 
 void mergeSort(int* arr, int start, int end){
@@ -13,6 +70,7 @@ void mergeSort(int* arr, int start, int end){
     }
 
     int mid = start+(end-start)/2;
+
     ///left part sort
     mergeSort(arr,start,mid);
 
@@ -25,8 +83,8 @@ void mergeSort(int* arr, int start, int end){
 
 int main(){
 
-    int arr[] = {4,13,5,2,9,2};
-    int n = 6;
+    int arr[] = {4,13,17,55,24,9,2};
+    int n = 7;
 
     int start = 0;
     int end = n-1;
