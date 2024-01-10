5ee75b18e3557ff1539015e26634d7ecf4b66ba0
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-10 15:01:20 +0530

    BackTracking

--- BackTracking.cpp/PermutationOfAString.cpp
+++ BackTracking.cpp/PermutationOfAString.cpp
@@ -4,7 +4,24 @@
 using namespace std;
 
 void printPermutation(string& str, int i){
-    
+    ///base case would be when we loop all the string
+    if(i>=str.length()){
+        cout<<str<<endl;
+        return;
+    }
+
+
+    //we apply the swapping logic
+    for(int j = i; j<str.length(); j++){
+        swap(str[j],str[i]);
+
+        ///as we already solve the one case so now we can make recursive calls
+        printPermutation(str,i+1);
+
+
+        //backtracking logic => to recreate the original string bcoz we send the string as pass by reference
+        swap(str[j], str[i]);
+    }
 }
 int main(){
 
