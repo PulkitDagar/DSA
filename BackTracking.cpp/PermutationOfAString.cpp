0f0b1bf36eebaded6b50c82cbab5f337bdf5197b
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-10 14:56:45 +0530

    BackTracking

---
+++ BackTracking.cpp/PermutationOfAString.cpp
@@ -0,0 +1,15 @@
+//Print all permutation of string
+//BackTracking is nothing but a specific form of recursion
+#include<iostream>
+using namespace std;
+
+void printPermutation(string& str, int i){
+    
+}
+int main(){
+
+    string str = "abc";
+    int i = 0;
+
+    printPermutation(str,i);
+}
