41b380aae00269fcc808b7898b307d888387a86f
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-17 09:02:52 +0530

    Oops

---
+++ Oops/class.cpp
@@ -0,0 +1,15 @@
+//Class is basically a blueprint of an object or we can say that a class is a user-defined data type
+// that has data members and member functions
+
+#include<iostream>
+using namespace std;
+
+class Animal {
+
+};
+
+int main(){
+    cout<<"the size of an empty class is"<<sizeof(Animal);
+    //the size of an empty class is 1;
+
+}
