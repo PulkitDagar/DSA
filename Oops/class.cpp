c1d12326e0491d93616c4b52d0d045a4927633fa
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-17 09:36:15 +0530

    concepts

--- Oops/class.cpp
+++ Oops/class.cpp
@@ -8,6 +8,7 @@ using namespace std;
 //syntax to create a class
 class Animal {
 
+    public:
     //state or properties
     int age;
     string name;
@@ -25,7 +26,15 @@ class Animal {
 };
 
 int main(){
-    cout<<"the size of an empty class is"<<sizeof(Animal);
+    // cout<<"the size of an empty class is"<<sizeof(Animal);
     //the size of an empty class is 1;
 
+    //object creation
+
+    //static memory allocation
+
+
+    Animal ramesh;
+    //cout<<"The age of ramesh is "<<ramesh.age<<endl; //it gives an error bcoz age is a private access modifier
+    cout<<"The age of ramesh is"<<ramesh.age<<endl; //it works bcoz we marked public in class
 }
