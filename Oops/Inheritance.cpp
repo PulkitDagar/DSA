091414c7ffdadb8fd8a8345e0fc789ee3e8ed120
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-18 10:13:18 +0530

    Inheritance

--- Oops/Inheritance.cpp
+++ Oops/Inheritance.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+//parent class
+class Animal {
+
+    int age;
+    int weight;
+
+    void eat(){
+        cout<<"Eating"<<endl;
+    }
+
+    void sleep(){
+        cout<<"Sleeping"<<endl;
+    }
+};
 
 int main(){
 
