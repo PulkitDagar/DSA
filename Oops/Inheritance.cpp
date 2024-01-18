e5f972dbf31ad68ceecf38f156b6ccf75d1a523b
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-18 10:17:29 +0530

    Inheritance

--- Oops/Inheritance.cpp
+++ Oops/Inheritance.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //parent class
 class Animal {
 
+    public:
     int age;
     int weight;
 
@@ -16,6 +17,17 @@ class Animal {
     }
 };
 
+//child class
+
+class Dog: public Animal{
+
+};
+
 int main(){
 
+    //create a object of child class and when running we see it already inherits the property of its parent class
+    Dog d1;
+    d1.eat();
+
+    return 0;
 }
