35b37c5a9195e0358e24afc974ce005b1f85e7ad
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-19 12:04:22 +0530

    Diamond problem

--- Oops/Multiple.cpp
+++ Oops/Multiple.cpp
@@ -1,16 +1,49 @@
 //Multiple Inheritance means when we have multiple parents and a single child and child inherit from both
 // of them
+// #include<iostream>
+// using namespace std;
+
+// class A{
+//     public:
+//     int physics;
+// };
+
+// class B {
+//     public:
+//     int chemistry;
+// };
+
+// class C: public A, public B{
+//     public:
+//     int maths;
+// };
+
+// int main(){
+
+//     C obj;
+//     cout<<obj.physics<<" "<<obj.chemistry<<" "<<obj.maths<<endl;
+//     return 0;
+// }
+
+//Diamond problem on multiple inheritance;
 #include<iostream>
 using namespace std;
 
 class A{
     public:
-    int physics;
+    int chemistry;
+    
+    A(){
+      chemistry = 110;
+    };
 };
 
 class B {
     public:
     int chemistry;
+    B(){
+      chemistry = 90;
+    }
 };
 
 class C: public A, public B{
@@ -21,6 +54,7 @@ class C: public A, public B{
 int main(){
 
     C obj;
-    cout<<obj.physics<<" "<<obj.chemistry<<" "<<obj.maths<<endl;
+    // cout<<obj.A::chemistry<<" "<<obj.maths<<endl;
+    cout<<obj.B::chemistry<<" "<<obj.maths<<endl;
     return 0;
 }
