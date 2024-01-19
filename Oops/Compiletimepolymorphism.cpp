abaf67fcf95e17e045867a44b45d16f806d96c7b
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-19 12:26:39 +0530

    compile time polymorphism

--- Oops/Compiletimepolymorphism.cpp
+++ Oops/Compiletimepolymorphism.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 
 
+///we make a many form of a single function
+class Maths{
+
+    public:
+    int sum(int a, int b){
+        return a+b;
+    };
+
+    int sum(int a, int b,int c){
+        return a+b+c;
+    };
+
+    int sum(int a, float b){
+        return a+b;
+    };
+};
+
 int main(){
 
+    Maths obj;
+    cout<<obj.sum(7,8);
     return 0;
 }
