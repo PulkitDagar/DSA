139fee68892e4cb574b23fd93709201be49ed9db
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-19 11:55:58 +0530

    New file

---
+++ Oops/Multiple.cpp
@@ -0,0 +1,10 @@
+//Multiple Inheritance means when we have multiple parents and a single child and child inherit from both
+// of them
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
