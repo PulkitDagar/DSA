dd40c96111dd78540ca70b064a2aa9549604ca4a
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-19 11:58:35 +0530

    changes

--- Oops/Multiple.cpp
+++ Oops/Multiple.cpp
@@ -3,8 +3,24 @@
 #include<iostream>
 using namespace std;
 
+class A{
+    public:
+    int physics;
+};
+
+class B {
+    public:
+    int chemistry;
+};
+
+class C: public A, public B{
+    public:
+    int maths;
+};
+
 int main(){
 
-
+    C obj;
+    cout<<obj.physics<<" "<<obj.chemistry<<" "<<obj.maths<<endl;
     return 0;
 }
