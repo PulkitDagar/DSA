e5afa1730f95fc74926ff37fbbec30df18317fb4
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-02-02 18:09:51 +0530

    changes

--- BackTracking.cpp/NQueenProblem.cpp
+++ BackTracking.cpp/NQueenProblem.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 
+
+
 int main(){
+    int n = 4;
+    vector<vector<int>>board(n, vector<int>(n,0));
+    int col = 0;
 
+    //0-> empty cell
+    //1 -> queen at the cell
 
+    solve(board,col,n);
 
     return 0;
 }
