c218d17dd17f9cd3fa0cecb41099a05a0b29b003
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-02-08 17:30:01 +0530

    changes

--- LinkedList/class1.cpp
+++ LinkedList/class1.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main(){
 
+/// creating node
+class Node {
+    public:
+
+    int data;
+    Node* next;
+
+    Node() {      ///creating constructor
+        this->data = 0;
+        this->next = NULL;
+    };
+
+    Node(int data){ /////parameterized constructor
+        this->data = data;
+        this->next = NULL;
+    };
+
+};
+
+int main(){
+    
+    Node* head = new Node();
 
 
     return 0;
