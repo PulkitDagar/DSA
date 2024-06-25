c28915f1d7fa072b762ccc88819d107fd64432d2
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-06-25 11:49:43 +0530

    stack

--- Stack/stack01.cpp
+++ Stack/stack01.cpp
@@ -2,31 +2,43 @@
 #include <stack>
 using namespace std;
 
-int main() {
+int main()
+{
 
-  //creation
+  // creation
   stack<int> st;
 
-  //insertion of element
+  // insertion of element
   st.push(10);
   st.push(20);
   st.push(30);
   st.push(40);
 
-  //remove
+  // remove
   st.pop();
 
-  //check element on top
-  cout<<"Element on top is"<<st.top()<<endl;
+  // check element on top
+  cout << "Element on top is" << st.top() << endl;
 
-  //size
-  cout<<"The size of stack is"<<st.size()<<endl;
+  // size
+  cout << "The size of stack is" << st.size() << endl;
 
-  //check empty
-  if(st.empty()){
-    cout<<"stack is empty"<<endl;
-  } else{
-    cout<<"stack is not empty"<<endl;
+  // check empty
+  if (st.empty())
+  {
+    cout << "stack is empty" << endl;
   }
+  else
+  {
+    cout << "stack is not empty" << endl;
+  }
+
+  // how to print stack
+  while (!st.empty())
+  {
+    cout << st.top() << " ";
+    st.pop();
+  }
+  cout << endl;
   return 0;
 }
