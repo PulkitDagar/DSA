513a3155fff079f1c674645a5386806cdc39df15
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-06-25 11:41:41 +0530

    changes

---
+++ Stack/stack01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+
+  //creation
+  stack<int> st;
+
+  //insertion of element
+  st.push(10);
+  st.push(20);
+  st.push(30);
+  st.push(40);
+
+  //remove
+  st.pop();
+
+  //check element on top
+  cout<<"Element on top is"<<st.top()<<endl;
+
+  //size
+  cout<<"The size of stack is"<<st.size()<<endl;
+
+  //check empty
+  if(st.empty()){
+    cout<<"stack is empty"<<endl;
+  } else{
+    cout<<"stack is not empty"<<endl;
+  }
+  return 0;
+}
