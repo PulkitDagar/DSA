fa81be060b85f2de13a6a352d079dcc49b0b6ff5
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-07-01 13:35:48 +0530

    changes

--- Stack/StackSTLClass.cpp
+++ Stack/StackSTLClass.cpp
@@ -79,3 +79,95 @@ int main() {
   return 0;
  
 }
+
+///second way
+#include <iostream>
+using namespace std;
+
+class Stack{
+  public:
+  int* arr;
+  int size;
+  int top;
+
+  Stack(int size){
+    arr = new int[size];
+    this->size = size;
+    this->top = -1;
+  };
+
+  //insertion
+  void push(int data){
+    if(top==size-1){
+      cout<<"Stack overflow"<<endl;
+      return;
+    }
+    else{
+      top++;
+      arr[top] = data;
+    }
+    
+  };
+
+  void pop(){
+    if(top==-1){
+      cout<<"stack underflow"<<endl;
+      return;
+    } else{
+      top--;
+    }
+
+  };
+
+  bool isEmpty(){
+    if(top==-1){
+      return true;
+    }
+    else{
+      return false;
+    }
+  };
+
+  //return top element
+  int getTop(){
+    if(top==-1){
+      cout<<"Stack is empty"<<endl;
+      return -1;
+    }
+    else{
+      return arr[top];
+    }
+
+  };
+
+  int getSize(){
+    return top+1;
+  }
+
+  void print(){
+    cout<<"top"<<top<<endl;
+    cout<<"gettop will be"<<getTop()<<endl;
+    cout<<"stack:";
+    for(int i=0; i<getSize(); i++){
+      cout<<arr[i]<<" ";
+
+    }
+    cout<<endl;
+  }
+};
+
+int main() {
+
+  //creation
+  Stack st(8);
+
+  //push
+  st.push(10);
+  st.push(20);
+  st.push(30);
+
+  st.print();
+
+  
+  return 0;
+}
