e4cde3ba07a9d3dcbf71e0d1e017a406b0c0a4a9
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-07-02 11:17:02 +0530

    changes

---
+++ Stack/Leetcode20.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0; i<s.length(); i++){
+            char ch = s[i];
+            if(ch == '(' || ch == '[' || ch == '{'){
+                //for open bracket just push
+                st.push(ch);
+            }
+            else{
+                if(!st.empty()){
+                    if(ch==')' && st.top()=='('){
+                        st.pop();
+                    }
+                    else if(ch=='}' && st.top() == '{'){
+                        st.pop();
+                    }
+                    else if(ch==']' && st.top() == '['){
+                        st.pop();
+                    }
+                    else{
+                        //no match
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        if(st.size() == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
