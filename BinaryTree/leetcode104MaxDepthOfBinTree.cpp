6c5fd46c913176c5ef2707e009dd31ad16bc5546
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-07-12 22:16:59 +0530

    completed

---
+++ BinaryTree/leetcode104MaxDepthOfBinTree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==nullptr){
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return max(leftDepth,rightDepth) + 1;
+    }
+};
