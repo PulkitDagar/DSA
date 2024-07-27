0e87c3f1e702c834e5fde0b6a663db7dc5ff38c1
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-07-27 10:19:00 +0530

    changes

---
+++ Heap/Heapify.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i){
+  int index = i;
+  int leftIndex = 2*i;
+  int rightIndex = 2*i+1;
+  int largest = index;
+  if(leftIndex < n && arr[largest] < arr[leftIndex]){
+    largest = leftIndex;
+  }
+
+  if(rightIndex < n && arr[largest] < arr[rightIndex]){
+    largest = rightIndex;
+  };
+
+  if(index !=largest){
+    //left ya right child mai se koi greater hogya
+    swap(arr[index], arr[largest]);
+
+    index = largest;
+
+    heapify(arr,n,index);
+  }
+}
+
+int main() {
+  
+  return 0;
+}
