d208ddb6fdf0ee61633e6a83d888f6e11edb5cdd
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-08-02 17:15:46 +0530

    completed

--- Heap/buildHeap.cpp
+++ Heap/buildHeap.cpp
@@ -6,11 +6,11 @@ void heapify(int arr[], int n, int i){
   int leftIndex = 2*i;
   int rightIndex = 2*i+1;
   int largest = index;
-  if(leftIndex < n && arr[largest] < arr[leftIndex]){
+  if(leftIndex <= n && arr[largest] < arr[leftIndex]){
     largest = leftIndex;
   }
 
-  if(rightIndex < n && arr[largest] < arr[rightIndex]){
+  if(rightIndex <= n && arr[largest] < arr[rightIndex]){
     largest = rightIndex;
   };
 
