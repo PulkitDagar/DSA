c3009bccdead5ce5ef352f3af52f4253126500cb
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-08-05 22:56:12 +0530

    changes

---
+++ Maps&Tries/STL(tries).cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class TrieNode{
+  public:
+  char data;
+  TrieNode* children[26];
+  bool isTerminal;
+
+  TrieNode(char d){
+    this->data = d;
+    for(int i=0; i<26; i++){
+      children[i] = NULL;
+    }
+    this->isTerminal = false;
+  }
+};
+
+void insertWord(TrieNode* root, string word){
+  cout<< "Inserting " << word << endl;
+  //base case
+  if(word.length() ==0){
+    root->isTerminal = true;
+    return;
+  }
+
+  char ch = word[0];
+  int index = ch - 'A';
+  TrieNode* child;
+  //present 
+  if(root->children[index] !=NULL){
+    child = root->children[index];
+  }
+  else{
+    //not present
+    child = new TrieNode(ch);
+    root->children[index] = child;
+  }
+  //recursion sambhal lega
+  insertWord(child, word.substr(1));
+}
+
+int main(){
+  TrieNode* root = new TrieNode('-');
+
+  insertWord(root,"coding");
+
+  return 0;
+}
