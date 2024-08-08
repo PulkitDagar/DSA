c62598e11d09f55fd89b40ae6bede435f6d6555c
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-08-08 14:14:13 +0530

    completed

---
+++ LinkedList/merge2sortedList.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* left, ListNode* right) {
+        if(left==0) return right;
+        if(right==0) return left;
+
+        ListNode* ans = new ListNode(-1);
+        ListNode* mptr = ans;
+
+        while(left && right){
+            if(left->val <= right->val){
+                mptr->next = left;
+                mptr = left;
+                left = left->next;
+            }
+            else{
+                mptr->next = right;
+                mptr = right;
+                right = right->next;
+            }
+        }
+
+        if(left){
+            mptr->next = left;
+
+        }
+
+        
+        if(right){
+            mptr->next = right;
+            // mptr = right;
+            // right = right->next;
+        }
+        return ans->next;
+    }
+};
